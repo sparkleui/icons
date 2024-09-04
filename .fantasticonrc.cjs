c0ecded26edd6c4f91b082a5b079207d7396955d
Daniele De Luca <48753350+danieledeluca@users.noreply.github.com>
2024-09-04 19:33:40 +0200

    chore: change fantasticon config format

---
+++ .fantasticonrc.cjs
@@ -0,0 +1,28 @@
+const { FontAssetType, OtherAssetType } = require('fantasticon');
+const codepoints = require('./src/template/sparkle-icons.json');
+
+/** @type {import('fantasticon').RunnerOptions} */
+module.exports = {
+    inputDir: './src/icons',
+    outputDir: './public/fonts',
+    fontTypes: [FontAssetType.WOFF, FontAssetType.WOFF2],
+    assetTypes: [OtherAssetType.JSON, OtherAssetType.SCSS],
+    name: 'sparkle-icons',
+    prefix: 'si',
+    codepoints,
+    fontsUrl: './fonts',
+    formatOptions: {
+        json: {
+            indent: 4,
+        },
+    },
+    templates: {
+        scss: './src/template/scss.hbs',
+    },
+    pathOptions: {
+        woff: './public/fonts/sparkle-icons.woff',
+        woff2: './public/fonts/sparkle-icons.woff2',
+        json: './src/template/sparkle-icons.json',
+        scss: './src/scss/abstracts/variables/_icons.scss',
+    },
+};
